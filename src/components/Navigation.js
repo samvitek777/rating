1ee8e41b708cef891b9c76964541b63461baf8a5
Victor Samoylenko <samvitek1@gmail.com>
2021-01-31 20:51:41 +0300

    TASK-2

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style/Navigation.css'
+
+const Navigation = () => {
+    return(
+        <nav className='nav'>
+            <div>2021</div>
+            <div>2020</div>
+            <div>2019</div>
+        </nav>
+    )
+}
+
+export default Navigation;
